c00e3e55df441feb3c70d971773fca477a53b834
Satwika Reddy Guntipally <satwikaguntipally333@gmail.com>
2024-06-19 03:34:03 +0000

    modified fct_orders

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -3,17 +3,12 @@ with orders as  (
 ),
 
 payments as (
-    select * from {{ ref ('stg_stripe__payments') }}
-),
-
-order_payments as (
-    select
-        order_id,
+    select order_id,
         sum (case when status = 'success' then amount end) as amount
-
-    from payments
+        from {{  ref('stg_stripe__payments') }}
     group by 1
 ),
+
 
  final as (
 
@@ -21,10 +16,10 @@ order_payments as (
         orders.order_id,
         orders.customer_id,
         orders.order_date,
-        coalesce (order_payments.amount, 0) as amount
+        coalesce (payments.amount, 0) as amount
 
     from orders
-    left join order_payments using (order_id)
+    left join payments using (order_id)
 )
 
 select * from final
